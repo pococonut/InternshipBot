99ec9b226e5f4efaa54fbe19bd7c59a8e265eb7c
Polina <polinalinana030@gmail.com>
2024-03-07 22:38:46 +0300

    Добавлена дата экспорта данных, изменен процесс добавления названий столбцов таблиц в файле export_data.py

--- InternshipBot/commands/export_data.py
+++ InternshipBot/commands/export_data.py
@@ -1,3 +1,4 @@
+import datetime
 import openpyxl
 from create import dp
 from aiogram import types
@@ -10,7 +11,8 @@ async def export(callback: types.CallbackQuery):
     """
     Функция возвращающая клавиатуру со списком данных, доступных для получения в виде excel-файла.
     """
-    await callback.message.edit_text("Выберите команду.", parse_mode='HTML', reply_markup=exp_ikb)
+
+    await callback.message.edit_text("Выберите команду.", reply_markup=exp_ikb)
 
 
 @dp.callback_query_handler(text='exp_task')
@@ -18,44 +20,39 @@ async def export_task(callback: types.CallbackQuery):
     """
     Функция возвращающая excel-файл с параметрами добавленных задач.
     """
-    tasks = select_task()
 
+    tasks = select_task()
     if not tasks:
-        await callback.message.edit_text("Данные отсутствуют.", parse_mode='HTML', reply_markup=exp_ikb)
+        await callback.message.edit_text("Данные отсутствуют.", reply_markup=exp_ikb)
     else:
         wb = openpyxl.Workbook()
         sheet = wb.active
         sheet.title = "Задачи"
+        columns = ["Сотрудник", "Студент", "Название", "Цель", "Описание", "Задачи",
+                   "Требования к технологиям", "Новые навыки", "Количество людей", "Материалы"]
 
-        c2 = sheet.cell(row=1, column=1)
-        c2.value = "Сотрудник"
-        c3 = sheet.cell(row=1, column=2)
-        c3.value = "Студент"
-        c4 = sheet.cell(row=1, column=3)
-        c4.value = "Название"
-        c5 = sheet.cell(row=1, column=4)
-        c5.value = "Цель"
-        c6 = sheet.cell(row=1, column=5)
-        c6.value = "Описание"
-        c7 = sheet.cell(row=1, column=6)
-        c7.value = "Задачи"
-        c8 = sheet.cell(row=1, column=7)
-        c8.value = "Требования к технологиям"
-        c9 = sheet.cell(row=1, column=8)
-        c9.value = "Новые навыки"
-        c10 = sheet.cell(row=1, column=9)
-        c10.value = "Количество людей"
-        c11 = sheet.cell(row=1, column=10)
-        c11.value = "Материалы"
+        for i in range(1, len(columns)+1):
+            c = sheet.cell(row=1, column=i)
+            c.value = columns[i-1]
 
+        last_idx = 0
         for i in range(2, len(tasks) + 2):
             index = i - 2
 
             s2 = sheet.cell(row=i, column=1)
-            s2.value = select_user(tasks[index].from_id).name if (tasks[index].from_id is not None and select_user(tasks[index].from_id) is not None) else None
+            worker = select_user(tasks[index].from_id)
+            task_from = tasks[index].from_id
+            if task_from and worker:
+                worker_name = worker.name
+            else:
+                worker_name = None
+            s2.value = worker_name
 
             s3 = sheet.cell(row=i, column=2)
-            s3.value = select_user(tasks[index].student_id).name if (tasks[index].student_id is not None and select_user(tasks[index].student_id) is not None) else None
+            if tasks[index].student_id and select_user(tasks[index].student_id):
+                s3.value = select_user(tasks[index].student_id).name
+            else:
+                s3.value = None
 
             s4 = sheet.cell(row=i, column=3)
             s4.value = tasks[index].task_name
@@ -81,6 +78,13 @@ async def export_task(callback: types.CallbackQuery):
             s11 = sheet.cell(row=i, column=10)
             s11.value = tasks[index].materials
 
+            last_idx = i + 2
+
+        s_data = sheet.cell(row=last_idx, column=1)
+        s_data.value = "Дата экспорта:"
+        s_data = sheet.cell(row=last_idx, column=2)
+        s_data.value = datetime.date.today()
+
         wb.save(r"files\Задачи.xlsx")
         doc = open(r"files\Задачи.xlsx", 'rb')
 
@@ -100,29 +104,17 @@ async def export_worker(callback: types.CallbackQuery):
         wb = openpyxl.Workbook()
         sheet = wb.active
         sheet.title = "Сотрудники"
-        f = 0
+        columns = ["Тип", "Имя", "Номер Телефона", "Дата регистрации", "Дата изменения", "Логин", "Пароль"]
 
-        c1 = sheet.cell(row=1, column=1)
-        c1.value = "Тип"
-        c2 = sheet.cell(row=1, column=2)
-        c2.value = "Имя"
-        c3 = sheet.cell(row=1, column=3)
-        c3.value = "Номер Телефона"
-        c4 = sheet.cell(row=1, column=4)
-        c4.value = "Дата регистрации"
-        c5 = sheet.cell(row=1, column=5)
-        c5.value = "Дата изменения"
-        c6 = sheet.cell(row=1, column=6)
-        c6.value = "Логин"
-        c7 = sheet.cell(row=1, column=7)
-        c7.value = "Пароль"
+        for i in range(1, len(columns)+1):
+            c = sheet.cell(row=1, column=i)
+            c.value = columns[i-1]
 
         i = 2
         for w in range(2, len(workers) + 2):
             index = w - 2
 
             if workers[index].type != 'student':
-                f = 1
                 s1 = sheet.cell(row=i, column=1)
                 s1.value = workers[index].type
 
@@ -146,6 +138,11 @@ async def export_worker(callback: types.CallbackQuery):
 
                 i += 1
 
+        s_data = sheet.cell(row=i + 1, column=1)
+        s_data.value = "Дата экспорта:"
+        s_data = sheet.cell(row=i+1, column=2)
+        s_data.value = datetime.date.today()
+
         wb.save(r"files\Сотрудники.xlsx")
         doc = open(r"files\Сотрудники.xlsx", 'rb')
 
@@ -162,35 +159,18 @@ async def export_applications(callback: types.CallbackQuery):
     wb = openpyxl.Workbook()
     sheet = wb.active
     sheet.title = "Заявки"
-    f = 0
+    columns = ["ФИО", "Номер Телефона", "ВУЗ", "Факультет", "Направление",
+               "Кафедра", "Курс", "Группа", "Курсовые", "Знания"]
 
-    c1 = sheet.cell(row=1, column=1)
-    c1.value = "ФИО"
-    c2 = sheet.cell(row=1, column=2)
-    c2.value = "Номер Телефона"
-    c3 = sheet.cell(row=1, column=3)
-    c3.value = "ВУЗ"
-    c4 = sheet.cell(row=1, column=4)
-    c4.value = "Факультет"
-    c5 = sheet.cell(row=1, column=5)
-    c5.value = "Направление"
-    c6 = sheet.cell(row=1, column=6)
-    c6.value = "Кафедра"
-    c7 = sheet.cell(row=1, column=7)
-    c7.value = "Курс"
-    c8 = sheet.cell(row=1, column=8)
-    c8.value = "Группа"
-    c9 = sheet.cell(row=1, column=9)
-    c9.value = "Курсовые"
-    c10 = sheet.cell(row=1, column=10)
-    c10.value = "Знания"
+    for i in range(1, len(columns) + 1):
+        c = sheet.cell(row=1, column=i)
+        c.value = columns[i - 1]
 
     i = 2
     for a in range(2, len(students) + 2):
         index = a - 2
 
         if students[index].type == 'student':
-            f = 1
 
             s1 = sheet.cell(row=i, column=1)
             s1.value = students[index].student_name
@@ -223,14 +203,17 @@ async def export_applications(callback: types.CallbackQuery):
             s10.value = students[index].knowledge
 
             i += 1
-    if not f:
-        await callback.message.edit_text("Данные отсутствуют.", parse_mode='HTML', reply_markup=exp_ikb)
-    else:
-        wb.save(r"files\Заявки.xlsx")
-        doc = open(r"files\Заявки.xlsx", 'rb')
 
-        await callback.answer()
-        await callback.message.answer_document(doc)
+    s_data = sheet.cell(row=i + 1, column=1)
+    s_data.value = "Дата экспорта:"
+    s_data = sheet.cell(row=i + 1, column=2)
+    s_data.value = datetime.date.today()
+
+    wb.save(r"files\Заявки.xlsx")
+    doc = open(r"files\Заявки.xlsx", 'rb')
+
+    await callback.answer()
+    await callback.message.answer_document(doc)
 
 
 @dp.callback_query_handler(text='exp_approved')
@@ -245,75 +228,61 @@ async def export_approved(callback: types.CallbackQuery):
         wb = openpyxl.Workbook()
         sheet = wb.active
         sheet.title = "Заявки"
+        columns = ["ФИО", "Номер Телефона", "ВУЗ", "Факультет", "Направление",
+                   "Кафедра", "Курс", "Группа", "Курсовые", "Знания"]
 
-        c1 = sheet.cell(row=1, column=1)
-        c1.value = "ФИО"
-        c2 = sheet.cell(row=1, column=2)
-        c2.value = "Номер Телефона"
-        c3 = sheet.cell(row=1, column=3)
-        c3.value = "ВУЗ"
-        c4 = sheet.cell(row=1, column=4)
-        c4.value = "Факультет"
-        c5 = sheet.cell(row=1, column=5)
-        c5.value = "Направление"
-        c6 = sheet.cell(row=1, column=6)
-        c6.value = "Кафедра"
-        c7 = sheet.cell(row=1, column=7)
-        c7.value = "Курс"
-        c8 = sheet.cell(row=1, column=8)
-        c8.value = "Группа"
-        c9 = sheet.cell(row=1, column=9)
-        c9.value = "Курсовые"
-        c10 = sheet.cell(row=1, column=10)
-        c10.value = "Знания"
-        f = 1
+        for i in range(1, len(columns) + 1):
+            c = sheet.cell(row=1, column=i)
+            c.value = columns[i - 1]
+
         i = 2
         for s in range(2, len(students) + 2):
             index = s - 2
 
             result = stud_approve(students[index].telegram_id)
-            if result is None:
-                f = 0
-            else:
-                if result[0]:
-                    s1 = sheet.cell(row=i, column=1)
-                    s1.value = students[index].student_name
 
-                    s2 = sheet.cell(row=i, column=2)
-                    s2.value = students[index].phone
+            if result:
+                s1 = sheet.cell(row=i, column=1)
+                s1.value = students[index].student_name
 
-                    s3 = sheet.cell(row=i, column=3)
-                    s3.value = students[index].university
+                s2 = sheet.cell(row=i, column=2)
+                s2.value = students[index].phone
 
-                    s4 = sheet.cell(row=i, column=4)
-                    s4.value = students[index].faculty
+                s3 = sheet.cell(row=i, column=3)
+                s3.value = students[index].university
 
-                    s5 = sheet.cell(row=i, column=5)
-                    s5.value = students[index].specialties
+                s4 = sheet.cell(row=i, column=4)
+                s4.value = students[index].faculty
 
-                    s6 = sheet.cell(row=i, column=6)
-                    s6.value = students[index].department
+                s5 = sheet.cell(row=i, column=5)
+                s5.value = students[index].specialties
 
-                    s7 = sheet.cell(row=i, column=7)
-                    s7.value = students[index].course
+                s6 = sheet.cell(row=i, column=6)
+                s6.value = students[index].department
 
-                    s8 = sheet.cell(row=i, column=8)
-                    s8.value = students[index].group
+                s7 = sheet.cell(row=i, column=7)
+                s7.value = students[index].course
 
-                    s9 = sheet.cell(row=i, column=9)
-                    s9.value = students[index].coursework
+                s8 = sheet.cell(row=i, column=8)
+                s8.value = students[index].group
 
-                    s10 = sheet.cell(row=i, column=10)
-                    s10.value = students[index].knowledge
+                s9 = sheet.cell(row=i, column=9)
+                s9.value = students[index].coursework
 
-                    i += 1
-        if f:
-            wb.save(r"files\Принятые студенты.xlsx")
-            doc = open(r"files\Принятые студенты.xlsx", 'rb')
-            await callback.answer()
-            await callback.message.answer_document(doc)
-        else:
-            await callback.message.edit_text("Данные отсутствуют.", parse_mode='HTML', reply_markup=exp_ikb)
+                s10 = sheet.cell(row=i, column=10)
+                s10.value = students[index].knowledge
+
+                i += 1
+
+        s_data = sheet.cell(row=i + 1, column=1)
+        s_data.value = "Дата экспорта:"
+        s_data = sheet.cell(row=i + 1, column=2)
+        s_data.value = datetime.date.today()
+
+        wb.save(r"files\Принятые студенты.xlsx")
+        doc = open(r"files\Принятые студенты.xlsx", 'rb')
+        await callback.answer()
+        await callback.message.answer_document(doc)
 
 
 @dp.callback_query_handler(text='exp_added')
@@ -328,15 +297,12 @@ async def export_added(callback: types.CallbackQuery):
         wb = openpyxl.Workbook()
         sheet = wb.active
         sheet.title = "Добавленные аккаунты"
+        columns = ["Логин", "Пароль", "Тип", "ФИО"]
+        last_idx = 0
 
-        c1 = sheet.cell(row=1, column=1)
-        c1.value = "Логин"
-        c2 = sheet.cell(row=1, column=2)
-        c2.value = "Пароль"
-        c3 = sheet.cell(row=1, column=3)
-        c3.value = "Тип"
-        c4 = sheet.cell(row=1, column=4)
-        c4.value = "ФИО"
+        for i in range(1, len(columns) + 1):
+            c = sheet.cell(row=1, column=i)
+            c.value = columns[i - 1]
 
         for i in range(2, len(added_users) + 2):
             index = i - 2
@@ -352,6 +318,13 @@ async def export_added(callback: types.CallbackQuery):
 
             s4 = sheet.cell(row=i, column=4)
             s4.value = added_users[index].name_usr
+
+            last_idx = i + 2
+
+        s_data = sheet.cell(row=last_idx, column=1)
+        s_data.value = "Дата экспорта:"
+        s_data = sheet.cell(row=last_idx, column=2)
+        s_data.value = datetime.date.today()
 
         wb.save(r"files\Добавленные аккаунты.xlsx")
         doc = open(r"files\Добавленные аккаунты.xlsx", 'rb')
