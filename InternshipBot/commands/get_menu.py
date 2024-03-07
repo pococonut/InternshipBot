5a02770aa614034dde44425723e04c9cb16cb36e
Polina <polinalinana030@gmail.com>
2024-03-07 22:50:17 +0300

    Проверка регистации для доступа к меню вынесена в отдельную функцию

--- InternshipBot/commands/get_menu.py
+++ InternshipBot/commands/get_menu.py
@@ -5,19 +5,29 @@ from commands.general import get_keyboard
 from keyboard import new_user_ikb, chat_ikb
 
 
+def check_get_menu(user_id):
+    """
+    Функция для проверки регистрации пользователя
+    :param user_id: Идентификатор пользователя в телеграм
+    :return: Текст сообщения, клавиатура
+    """
+
+    user_exist = user_type(user_id)
+    if not user_exist:
+        msg_text = (f'Выберите команду.\n<em>Регистрация</em> -  Для студентов.'
+                    f'\n<em>Авторизация</em> - Для сотрудников.')
+        return msg_text, new_user_ikb
+    return 'Выберите команду.', get_keyboard(user_id)
+
+
 @dp.message_handler(commands=['menu'])
 async def menu_get(message: types.Message):
     """
     Функция получения меню для зарегистрированного пользователя и авторизации/регистрации для незарегистрированного.
     """
-    user_exist = user_type(message.from_user.id)
-    if not user_exist:
-        await message.answer(
-            f'Выберите команду.\n<em>Регистрация</em> -  Для студентов.\n'
-            f'<em>Авторизация</em> - Для сотрудников.', parse_mode='HTML', reply_markup=new_user_ikb)
-    else:
-        keyboard = get_keyboard(message.from_user.id)
-        await message.answer(f'Выберите команду.', parse_mode='HTML', reply_markup=keyboard)
+
+    msg_text, keyboard = check_get_menu(message.from_user.id)
+    await message.answer(msg_text, parse_mode='HTML', reply_markup=keyboard)
 
 
 @dp.callback_query_handler(text='menu')
@@ -25,8 +35,9 @@ async def menu_get_inline(callback: types.CallbackQuery):
     """
     Функция получения меню для зарегистрированного пользователя и авторизации/регистрации для незарегистрированного.
     """
-    keyboard = get_keyboard(callback.from_user.id)
-    await callback.message.edit_text("Выберите команду.", parse_mode='HTML', reply_markup=keyboard)
+
+    msg_text, keyboard = check_get_menu(callback.from_user.id)
+    await callback.message.edit_text(msg_text, parse_mode='HTML', reply_markup=keyboard)
 
 
 @dp.callback_query_handler(text='chat')
@@ -34,5 +45,6 @@ async def chat_command(callback: types.CallbackQuery):
     """
     Функция возвращающая ссылку на общий чат.
     """
-    await callback.message.edit_text("Чат для связи доступен по ссылке - https://t.me/+FShhqiWUDJRjODky.",
-                                     disable_web_page_preview=True, reply_markup=chat_ikb)
+
+    msg_text = "Чат для связи доступен по ссылке - https://t.me/+FShhqiWUDJRjODky."
+    await callback.message.edit_text(msg_text, disable_web_page_preview=True, reply_markup=chat_ikb)
