252c7f64d194591747d3d3ec7250ef17a42312a3
Polina <polinalinana030@gmail.com>
2024-04-24 21:28:55 +0300

    Изменен файл requirements.txt, добавлены файлы Dockerfile, docker-compose.yml

--- InternshipBot/config.py
+++ InternshipBot/config.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
@@ -9,8 +9,8 @@ load_dotenv()
 class Settings(BaseSettings):
     api: str = os.getenv("API")
     host: str = os.getenv("HOST")
-    password: str = os.getenv("DB_PASSWORD")
-    database: str = os.getenv("DB_NAME")
+    password: str = os.getenv("POSTGRES_PASSWORD")
+    database: str = os.getenv("POSTGRES_DB")
 
 
 settings = Settings()
